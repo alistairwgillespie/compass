b2411f959101b458c03d73e316f86948e47de924
Alistair Gillespie <alistair@howlingweb.com.au>
2020-06-22 18:14:08 +1000

    code: added exercise and workout models

--- app/models.py
+++ app/models.py
@@ -4,15 +4,73 @@ from app import db
 users_programs = db.Table(
     'users_programs',
     db.Column(
+        'user_id',
+        db.Integer,
+        db.ForeignKey('user.id'),
+        primary_key=True),
+    db.Column(
         'program_id',
         db.Integer,
         db.ForeignKey('program.id'),
         primary_key=True),
     db.Column(
-        'user_id',
+        'timestamp',
+        db.DateTime,
+        index=True,
+        default=datetime.utcnow))
+
+programs_workouts = db.Table(
+    'programs_workouts',
+    db.Column(
+        'program_id',
         db.Integer,
-        db.ForeignKey('user.id'),
-        primary_key=True))
+        db.ForeignKey('program.id'),
+        primary_key=True),
+    db.Column(
+        'workout_id',
+        db.Integer,
+        db.ForeignKey('workout.id'),
+        primary_key=True),
+    db.Column(
+        'week',
+        db.Integer),
+    db.Column(
+        'day_of_week',
+        db.Integer),
+    db.Column(
+        'am',
+        db.Boolean),
+    db.Column(
+        'pm',
+        db.Boolean))
+
+workouts_exercises = db.Table(
+    'workouts_exercises',
+    db.Column(
+        'workout_id',
+        db.Integer,
+        db.ForeignKey('workout.id'),
+        primary_key=True),
+    db.Column(
+        'exercise_id',
+        db.Integer,
+        db.ForeignKey('exercise.id'),
+        primary_key=True),
+    db.Column(
+        'sets',
+        db.Integer),
+    db.Column(
+        'reps',
+        db.Integer),
+    db.Column(
+        'rest',
+        db.String(64)),
+    db.Column(
+        'duration',
+        db.Integer),
+    db.Column(
+        'effort',
+        db.Integer))
 
 
 class User(db.Model):
@@ -41,6 +99,39 @@ class Program(db.Model):
     goal = db.Column(db.String(140))
     description = db.Column(db.String(140))
     timestamp = db.Column(db.DateTime, index=True, default=datetime.utcnow)
+    # Relationships
+    programs_workouts = db.relationship(
+        'Workout',
+        secondary=programs_workouts,
+        lazy='subquery',
+        backref=db.backref(
+            'programs',
+            lazy=True))
 
     def __repr__(self):
         return '<Program {}>'.format(self.title)
+
+
+class Workout(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    workout_type = db.Column(db.String(64))
+    # Relationships
+    workouts_exercises = db.relationship(
+        'Exercise',
+        secondary=workouts_exercises,
+        lazy='subquery',
+        backref=db.backref(
+            'workouts',
+            lazy=True))
+
+    def __repr__(self):
+        return '<Workout {}>'.format(self.title)
+
+
+class Exercise(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    exercise_title = db.Column(db.String(64))
+    exercise_type = db.Column(db.String(64))
+
+    def __repr__(self):
+        return '<Exercise {}>'.format(self.title)
--- compass.py
+++ compass.py
@@ -1,1 +1,12 @@
-from app import app
+from app import app, db
+from app.models import User, Program, Workout, Exercise
+
+
+@app.shell_context_processor
+def make_shell_context():
+    return {
+        'db': db,
+        'User': User,
+        'Program': Program,
+        'Workout': Workout,
+        'Exercise': Exercise}
