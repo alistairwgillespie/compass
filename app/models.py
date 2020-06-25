82f1392249f21bec37652ee612ba110e6304c382
Alistair Gillespie <alistair@howlingweb.com.au>
2020-06-25 21:44:37 +1000

    code: added user login, register and logout with hash passwords

--- app/models.py
+++ app/models.py
@@ -1,5 +1,14 @@
 from datetime import datetime
 from app import db
+from app import login
+from werkzeug.security import generate_password_hash, check_password_hash
+from flask_login import UserMixin
+
+
+@login.user_loader
+def load_user(id):
+    return User.query.get(int(id))
+
 
 users_programs = db.Table(
     'users_programs',
@@ -73,7 +82,7 @@ workouts_exercises = db.Table(
         db.Integer))
 
 
-class User(db.Model):
+class User(UserMixin, db.Model):
     id = db.Column(db.Integer, primary_key=True)
     first_name = db.Column(db.String(64))
     last_name = db.Column(db.String(64))
@@ -88,6 +97,12 @@ class User(db.Model):
         backref=db.backref(
             'users',
             lazy=True))
+
+    def set_password(self, password):
+        self.password_hash = generate_password_hash(password)
+
+    def check_password(self, password):
+        return check_password_hash(self.password_hash, password)
 
     def __repr__(self):
         return '<User {}>'.format(self.username)
