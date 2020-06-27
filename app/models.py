4e5a926902485794f02f984a61332e3e5a94dd3f
Alistair Gillespie <alistair@howlingweb.com.au>
2020-06-27 18:53:50 +1000

    code: added posts table

--- app/models.py
+++ app/models.py
@@ -5,6 +5,7 @@ from werkzeug.security import generate_password_hash, check_password_hash
 from flask_login import UserMixin
 from hashlib import md5
 
+
 @login.user_loader
 def load_user(id):
     return User.query.get(int(id))
@@ -144,6 +145,16 @@ class User(UserMixin, db.Model):
 
     def __repr__(self):
         return '<User {}>'.format(self.username)
+
+
+class Post(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    body = db.Column(db.String(140))
+    timestamp = db.Column(db.DateTime, index=True, default=datetime.utcnow)
+    user_id = db.Column(db.Integer, db.ForeignKey('user.id'))
+
+    def __repr__(self):
+        return '<Post {}>'.format(self.body)
 
 
 class Program(db.Model):
